cdafe73d39a9d12940997f74f7b6fb65631a5f7f
uvorbs <yuval@orbs.com>
2024-09-26 11:43:42 +0300

    add non auth healthcheck

--- src/index.ts
+++ src/index.ts
@@ -17,6 +17,11 @@ async function main() {
   if (createUserIndex !== -1 && args[createUserIndex + 1]) {
     const username = args[createUserIndex + 1];
     try {
+      if (!username) {
+        console.log(`Username was not provided`);
+        process.exit(1);
+      }
+
       const apiKey = await generateApiKey(username);
       console.log(`User '${username}' created with API Key: ${apiKey}`);
       process.exit(0);
@@ -30,8 +35,6 @@ async function main() {
 
     // Middleware to parse JSON
     app.use(express.json());
-    // Apply the middleware to all routes
-    app.use(apiKeyAuth);
 
     const wrapper = new Wrapper();
 
@@ -44,57 +47,15 @@ async function main() {
       logger.error(`${status} ${msg}`);
       res.status(status).send(msg);
     }
-    // GET /api/rfq/pairprice?chainid=137&pair=WETH/USDT&amount=10&side=1&isbase=1' --header 'x-apikey: API_KEY'
-    // app.get("/api/rfq/pairprice", async (req: Request, res: Response) => {
-    //   const { chainid, pair, amount, side, isbase } = req.query;
-    //   const apiKey = req.header("x-api-key");
-    //   if (!chainid) {
-    //     return sendErr(res, 500, "chainId is missing");
-    //   }
-    //   if (!pair) {
-    //     return sendErr(res, 500, "pair is missing");
-    //   }
-    //   if (!amount) {
-    //     return sendErr(res, 500, "amount is missing");
-    //   }
-    //   if (!side) {
-    //     return sendErr(res, 500, "side is missing");
-    //   }
-    //   if (!isbase) {
-    //     return sendErr(res, 500, "isBase is missing");
-    //   }
-    //   const quote = await wrapper.quoteAuction(
-    //     chainid as string,
-    //     pair as string,
-    //     amount as string,
-    //     side as string,
-    //     isbase === "1",
-    //   );
-    //   // error
-    //   if (quote.error) {
-    //     return sendErr(res, 500, "quoteAuction exception: " + quote.error);
-    //   }
-
-    //   // You can implement your own logic to validate the apiKey or check query params  
-    //   const response = {
-    //     success: true,
-    //     pair: pair,
-    //     side: side, // 0 = buy A | 1 = sell A
-    //     price: quote.price,
-    //     baseAmount: isbase ? amount : quote.outAmount,
-    //     quoteAmount: !isbase ? amount : quote.outAmount
-    //   };
-
-    //   res.json(response);
-    // });
-
-
+    // health - no AUTH
+    app.get('/health', (req, res) => {
+      res.status(200).json({ status: 'OK' });
+    });
 
     // GET /api/rfq/prices
     // curl --location 'https://api.dexalot.com/api/rfq/prices?chainid=43114' --header 'x-apikey: API_KEY'
-    app.get("/api/rfq/prices", (req: Request, res: Response) => {
+    app.get("/api/rfq/prices", apiKeyAuth, (req: Request, res: Response) => {
       const { chainid } = req.query;
-      const apiKey = req.header("x-api-key");
       if (!chainid) {
         return sendErr(res, 500, "chainId is missing");
       }
@@ -104,8 +65,7 @@ async function main() {
     });
 
     // POST /api/rfq/firm
-    app.post("/api/rfq/firm", async (req: Request, res: Response) => {
-      const apiKey = req.header("x-api-key");
+    app.post("/api/rfq/firm", apiKeyAuth, async (req: Request, res: Response) => {
       const requiredFields = ['chainid', 'takerAsset', 'makerAsset', 'takerAmount', 'userAddress'];
 
       for (const field of requiredFields) {
@@ -120,9 +80,52 @@ async function main() {
       }
       res.json(response);
     });
-
   }
 }
+
+// GET /api/rfq/pairprice?chainid=137&pair=WETH/USDT&amount=10&side=1&isbase=1' --header 'x-apikey: API_KEY'
+// app.get("/api/rfq/pairprice", apiKeyAuth,async (req: Request, res: Response) => {
+//   const { chainid, pair, amount, side, isbase } = req.query;
+//   const apiKey = req.header("x-api-key");
+//   if (!chainid) {
+//     return sendErr(res, 500, "chainId is missing");
+//   }
+//   if (!pair) {
+//     return sendErr(res, 500, "pair is missing");
+//   }
+//   if (!amount) {
+//     return sendErr(res, 500, "amount is missing");
+//   }
+//   if (!side) {
+//     return sendErr(res, 500, "side is missing");
+//   }
+//   if (!isbase) {
+//     return sendErr(res, 500, "isBase is missing");
+//   }
+//   const quote = await wrapper.quoteAuction(
+//     chainid as string,
+//     pair as string,
+//     amount as string,
+//     side as string,
+//     isbase === "1",
+//   );
+//   // error
+//   if (quote.error) {
+//     return sendErr(res, 500, "quoteAuction exception: " + quote.error);
+//   }
+
+//   // You can implement your own logic to validate the apiKey or check query params  
+//   const response = {
+//     success: true,
+//     pair: pair,
+//     side: side, // 0 = buy A | 1 = sell A
+//     price: quote.price,
+//     baseAmount: isbase ? amount : quote.outAmount,
+//     quoteAmount: !isbase ? amount : quote.outAmount
+//   };
+
+//   res.json(response);
+// });
 
 // Call the main function
 main();
